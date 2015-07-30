5bc12bcec20f04d13b00cfcbb3d7e7a97b1c55ca
gcpatton <grzcorpa@gmail.com>
2015-07-30 12:18:48 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+##Tibia-leaflet-map
+
+This is a small example of how to display the Tibia map with leaflet.js
+
+Big thanks to [commenthol/gdal2tiles-leaflet](https://github.com/commenthol/gdal2tiles-leaflet)
+
+
+##TODO
+
+Ideas
+-----
+
+ * Custom markers
+ * Backend for multi-user functionality (Sharing map instances, markers, etc..)
---
+++ js/main.js
@@ -0,0 +1,42 @@
+/** Thanks to commenthol/gdal2tiles-leaflet **/
+
+jQuery(document).ready(function($) {
+
+	var floors = new Array(15);
+
+	$("#floor").on("change", function(){
+	    floors[$(this).val()].bringToFront();
+	});
+		
+	    var minZoom = 3,
+	        maxZoom = 7,
+	        img = [
+	            2048,
+	            2304
+	        ];
+
+	    map = L.map('map', {
+	        minZoom: minZoom,
+	        maxZoom: maxZoom
+	    });
+
+	    rc = new L.RasterCoords(map, img);
+	    rc.setMaxBounds();
+
+	    map.setView(rc.unproject([img[0] / 2, img[1] / 2]), 4);
+	    var layerBounds = L.layerGroup([
+	        L.marker(rc.unproject([0, 0])).bindPopup('[0,0]'),
+	        L.marker(rc.unproject(img)).bindPopup(JSON.stringify(img))
+	    ]);
+	    map.addLayer(layerBounds);
+
+	    for(i=0; i < floors.length; ++i){
+	        floors[i] = 
+	        L.tileLayer('img/mapfloor-'+i+'/{z}/{x}/{y}.png', {
+	                noWrap: true
+	            });
+	        map.addLayer(floors[i]);
+	    }
+
+	floors[7].bringToFront();
+});
